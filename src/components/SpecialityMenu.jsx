29b888b31854d59807af458b4986b9fbfa6f69d5
SmithMaharjan <smith.maharjan1234@gmail.com>
2024-10-04 20:39:23 +0545

    speciality menu

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const SpecialityMenu = () => {
     return (
-        <div id='speciality' className=' flex flex-col items-center py-16 text-gray-800 gap-2'>
+        <div id='speciality' className=' flex flex-col items-center pt-20 text-gray-800 gap-2'>
             <h1 className=' text-2xl md:text-3xl font-medium  '>Find By Speciality</h1>
             <p className=' sm:w-1/2 text-center text-sm'>simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free </p>
             <div className=' flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
